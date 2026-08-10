7921bcaf80cbacb981b82567513bff88ffdc7c48
Kevixx <kevoboc@gmail.com>
2026-08-10 15:27:41 +0200

    born besilent

--- Frontend/src/app/app.ts
+++ Frontend/src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.scss',
 })
 export class App {
-  protected readonly title = signal('pl-and-partners-frontend');
+  protected readonly title = signal('besilent-frontend');
 }
--- Frontend/src/environments/environment.ts
+++ Frontend/src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  url: 'https://pl-and-partners.vercel.app',
-  apiUrl: 'https://pl-and-partners-production.up.railway.app/api',
+  url: 'https://besilent.vercel.app',
+  apiUrl: 'https://besilent.up.railway.app/api',
   supabaseUrl: 'https://klxqudikgnngioanwfyf.supabase.co',
   supabaseKey: 'sb_publishable_4cqr8Qk0uY28XhJVPo4wRQ_UvDFwQvY',
 };
