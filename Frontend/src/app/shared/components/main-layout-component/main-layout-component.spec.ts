c2042f613bbc1d486765304fa9923cc2b2109dc2
Kevixx <kevoboc@gmail.com>
2026-08-20 11:45:00 +0200

    fix testing

--- Frontend/src/app/shared/components/main-layout-component/main-layout-component.spec.ts
+++ Frontend/src/app/shared/components/main-layout-component/main-layout-component.spec.ts
@@ -1,6 +1,11 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { MainLayoutComponent } from './main-layout-component';
+
+// Import the testing providers
+import { provideRouter } from '@angular/router';
+import { provideTranslateService } from '@ngx-translate/core';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 
 describe('MainLayoutComponent', () => {
   let component: MainLayoutComponent;
@@ -9,6 +14,15 @@ describe('MainLayoutComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [MainLayoutComponent],
+      providers: [
+        // Provide a dummy router to fix the ActivatedRoute error
+        provideRouter([]),
+
+        // Prevent the Navbar inside the layout from crashing
+        provideTranslateService(),
+        provideHttpClient(),
+        provideHttpClientTesting(),
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(MainLayoutComponent);
